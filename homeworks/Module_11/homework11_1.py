2465521af60a9d288b4ab8bb7541d7f33da04cc6
anitova11 <anitova.k.v@mail.ru>
2024-08-20 21:16:16 +0300

    homework11_1 done

---
+++ homeworks/Module_11/homework11_1.py
@@ -0,0 +1,34 @@
+import datetime
+import multiprocessing
+
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r', encoding='utf-8') as file:
+        line = file.readline()
+        while line != '':
+            all_data.append(line)
+            line = file.readline()
+
+
+files = []
+for i in range(4):
+    files.append(f'./numbers/file_{i + 1}.txt')
+
+# start = datetime.datetime.now()
+# for i in range(1, 5):
+#     read_info(f'./numbers/file_{i}.txt')
+# end = datetime.datetime.now()
+# print(end - start)
+
+# 0:00:05.360205
+
+
+if __name__ == '__main__':
+    with multiprocessing.Pool(processes=6) as pool:
+        start = datetime.datetime.now()
+        pool.map(read_info, files)
+    end = datetime.datetime.now()
+    print(end - start)
+
+# 0:00:02.273405
